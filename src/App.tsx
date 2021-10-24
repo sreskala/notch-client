e7c7b990d5a8c0874b3d9ac81843753747162de2
sreskala <sam.sagas@hotmail.com>
2021-10-24 17:06:43 -0400

    navbar and some initial post breakdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-import axios from 'axios'
-import { Container, Typography, Box, Slider } from '@mui/material';
-import { Post } from './types/post';
+import axios from "axios";
+import { Container, Typography, Box, Slider } from "@mui/material";
+import { Post } from "./types/post";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
-
+  const [loading, setLoading] = useState<boolean>();
   const [posts, setPosts] = useState<Post[]>([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/posts').then(res => {
-      const postResponse = (res.data) as Post[];
+    setLoading(true);
+    axios.get("http://localhost:5000/api/posts").then((res) => {
+      const postResponse = res.data as Post[];
       setPosts(postResponse);
-    })
-  }, [])
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
 
   return (
     <div className="App">
-      {
-        posts && posts.map((post: Post) => (
-          <h5>{post.title}</h5>
-        ))
-      }
+      <Navbar name="sam" />
       <Container maxWidth="md">
         <Typography variant="h4" component="h1" gutterBottom>
-          quick test for MUI
+          Notch App
         </Typography>
+
+        {posts && posts.map((post: Post) => <h5>{post.title}</h5>)}
 
         <Box width={300} alignContent="center">
           <Slider
@@ -34,7 +39,7 @@ function App() {
             defaultValue={65}
             aria-label="Small"
             valueLabelDisplay="auto"
-            />
+          />
         </Box>
       </Container>
     </div>
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,9 @@
 export const APP_TITLE = 'Notchat Nora'
 
 export const FOOTER_TEXT = `${new Date().getFullYear()} || Built by Sam Reskala`
+
+export const COLORS = {
+    baseColor: '#F5F0F6',
+    primaryColor: '#197278',
+    secondaryColor: '#80FFE8'
+}
