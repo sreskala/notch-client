7425d1a82e0a40f006302dc26efa8ecb45d551d4
sreskala <sam.sagas@hotmail.com>
2021-10-24 12:09:21 -0400

    added types

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,27 @@ import './App.css';
 
 import axios from 'axios'
 import { Container, Typography, Box, Slider } from '@mui/material';
+import { Post } from './types/post';
 
 function App() {
 
-  const [posts, setPosts] = useState<any>();
+  const [posts, setPosts] = useState<Post[]>([]);
 
   useEffect(() => {
     axios.get('http://localhost:5000/api/posts').then(res => {
-      setPosts(res.data)
+      const postResponse = (res.data) as Post[];
+      setPosts(postResponse);
     })
   }, [])
 
   return (
     <div className="App">
       {
-        posts && posts.map((post: any) => (
+        posts && posts.map((post: Post) => (
           <h5>{post.title}</h5>
         ))
       }
-      <Container maxWidth="sm">
+      <Container maxWidth="md">
         <Typography variant="h4" component="h1" gutterBottom>
           quick test for MUI
         </Typography>
